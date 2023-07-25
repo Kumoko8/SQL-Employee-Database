701449fdff7683a20d918a465165d98f766535ab
Kumori <tauen8@gmail.com>
2023-07-25 12:28:08 -0500

    configured table and seed data structure with foreign keys

--- develop/db/schema.sql
+++ develop/db/schema.sql
@@ -4,21 +4,23 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-  id INT NOT NULL,
-  name VARCHAR(30) NOT NULL,
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  department_name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE role (
-  id INT NOT NULL,
+CREATE TABLE employee_role (
+  role_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary decimal NOT NULL,
-  department_id INT NOT NULL FOREIGN KEY REFERENCES department(id)
+  department_id INT NOT NULL, 
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-  id INT NOT NULL,
+  employee_id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL FOREIGN KEY REFERENCES role(id),
-  manager_id INT NOT NULL FOREIGN KEY REFERENCES employee(id)
+  role_id INT NOT NULL, 
+  FOREIGN KEY (role_id) REFERENCES employee_role(role_id),
+  manager_id INT
 );
--- develop/db/seeds.sql
+++ develop/db/seeds.sql
@@ -1,1 +1,17 @@
+INSERT INTO department (department_name)
+VALUES ('Science'),
+        ('Administration'),
+        ('English');
 
+INSERT INTO employee_role (title, salary, department_id)
+VALUES ('Principal', 100000, 2),
+        ('Teacher', 75000, 1),
+        ('Aid', 60000, 3 );
+    
+INSERT INTO employee (employee_id, first_name, last_name, role_id, manager_id)
+VALUES (1, 'Jim', 'Halpert', 2, null),
+        (2, 'Kevin', 'Malone', 3, 3),
+        (3, 'Angela', 'Martin', 1, 1);
+    
+ALTER TABLE employee
+ADD FOREIGN KEY (manager_id) REFERENCES employee(employee_id);
